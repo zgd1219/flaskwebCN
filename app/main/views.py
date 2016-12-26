27811eb009bfa0226075248be2d3a3ee8185de71
Zhou Guangda <anotherzgd@sina.cn>
2016-12-26 19:33:00 +0800

    完成博客文章编辑功能 添加博文编辑页面edit_post.html，修改_posts.html添加编辑博文的链接按钮 添加一个编辑博客文章的路由edit(id)

--- app/main/views.py
+++ app/main/views.py
@@ -80,4 +80,18 @@ def post(id):
     post = Post.query.get_or_404(id)
     return render_template('post.html', posts=[post])
 
-
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and \
+        not current_user.is_administrator():
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body=form.body.data
+        db.session.add(post)
+        flash("文章编辑成功!")
+        return redirect(url_for("main.post", id=post.id))
+    form.body.data=post.body
+    return render_template("edit_post.html", form=form)
