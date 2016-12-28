918ed45da54be4739061588380c1c81bbc51be10
Zhou Guangda <anotherzgd@sina.cn>
2016-12-28 16:28:40 +0800

    测试wtform render_kw添加placeholder

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,7 +5,8 @@ from ..models import User
 
 class LoginForm(FlaskForm):
     email = StringField('邮箱', validators=[DataRequired(message='邮箱不能为空'),
-                                          Length(1,64),Email(message='请输入正确的邮箱格式！')])
+                                          Length(1,64),Email(message='请输入正确的邮箱格式！')],
+                                            render_kw={"placeholder": "example@xxx.com"})
     password = PasswordField('密码', validators=[DataRequired(message='密码不能为空！')])
     remember_me = BooleanField('记住我')
     submit = SubmitField('登录')
